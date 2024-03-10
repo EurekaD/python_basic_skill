a3f98d97e8126e91e05aa50155310e7a3d632620
lin.chen <ZqKno@2023>
2024-03-10 17:56:35 +0800

    [2024-3-10] 装饰器实现单例模式设计，用于web应用中的ai模型加载

---
+++ 单例模式.py
@@ -0,0 +1,28 @@
+"""
+类似于 全局变量 ，
+
+"""
+
+
+def singleton(cls):
+    _instance = {}
+
+    def inner():
+        if cls not in _instance:
+            _instance[cls] = cls()
+        return _instance[cls]
+
+    return inner
+
+
+@singleton
+class Cls(object):
+    def __init__(self):
+        pass
+
+
+cls1 = Cls()
+cls2 = Cls()
+print(id(cls1) == id(cls2))
+
+
--- 装饰器.py
+++ 装饰器.py
@@ -1,10 +1,27 @@
 """
 在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）
 
+装饰器是 Python 中一种强大而灵活的功能，用于修改或扩展函数或类的行为。
+它允许您在不修改原始代码的情况下包装或修改函数或方法。
+装饰器通常用于添加新的功能、修改参数或结果，或者进行性能测量、日志记录等操作。
+在 Python 中，装饰器是函数或类，它们接受一个函数作为输入并返回一个新的函数或修改后的类。
+装饰器通过在要装饰的函数或类之前使用 @decorator_name 语法来应用。
 
 """
 
 
-# 现在，假设我们要增强now()函数的功能，比如，在函数调用前后自动打印日志，但又不希望修改now()函数的定义，
-# 这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）。
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    print("Test")
+    return wrapper
+
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
 
